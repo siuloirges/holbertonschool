e0f9505806136ece8e84538861bee191de11b409
Sergio <sergiovegam41@gmail.com>
2022-05-07 21:59:50 -0500

    holbertonschool

---
+++ js/index.js
@@ -0,0 +1,35 @@
+document.querySelector('#Flutter').addEventListener("mouseenter", function(e){
+    console.log("Hola Flutter");
+    let Flutter = new Audio('sonds/Flutter.wav');
+    Flutter.play();
+});
+
+document.querySelector('#Laravel').addEventListener("mouseenter", function(e){
+    console.log("Hola Laravel");
+    let Laravel = new Audio('sonds/Laravel.wav');
+    Laravel.play();
+});
+
+document.querySelector('#JavaScript').addEventListener("mouseenter", function(e){
+    console.log("Hola JavaScript");
+    let JavaScript = new Audio('sonds/JavaScript.wav');
+    JavaScript.play();
+});
+
+document.querySelector('#mongodb').addEventListener("mouseenter", function(e){
+    console.log("Hola mongodb");
+    let mongodb = new Audio('sonds/mongodb.wav');
+    mongodb.play();
+});
+
+document.querySelector('#php').addEventListener("mouseenter", function(e){
+    console.log("Hola php");
+    let php = new Audio('sonds/php.wav');
+    php.play();
+});
+
+document.querySelector('#python').addEventListener("mouseenter", function(e){
+    console.log("Hola python");
+    let python = new Audio('sonds/python.wav');
+    python.play();
+});
